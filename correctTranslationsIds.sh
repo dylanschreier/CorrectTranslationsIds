30525fbb236f02ec4c0a95c80742f0cafc539115
DylanSchreier <dylan.schreier83@gmail.com>
2017-06-20 16:09:42 +0200

    Correct script

--- correctTranslationsIds.sh
+++ correctTranslationsIds.sh
@@ -2,13 +2,15 @@
 
 correctTranslationsIds() {
     if [ $# -eq 1 ]; then
-        awk '{for(x=1;x<=NF;x++)if($x~/id="[0-9]+"/){sub(/[0-9]+/,++i)}}1' $1
+        awk '{for(x=1;x<=NF;x++)if($x~/id="[0-9]+"/){sub(/[0-9]+/,++i)}}1' $1 > $1.tmp
+        rm $1
+        mv $1.tmp $1
     fi
 }
 
 correctAllTranslationsFiles() {
     if [ $# -eq 1 ]; then
-        TRANSLATIONS_FILENAMES=$(find $1/Resourcers/translations/ -regextype posix-extended -regex '^.*\.xlf')
+        TRANSLATIONS_FILENAMES=$(find $1/Resources/translations/ -regextype posix-extended -regex '^.*\.xlf')
         while read -r filename ; do
             echo "Correct translations ids for file $filename"
             correctTranslationsIds $filename
@@ -19,6 +21,6 @@ correctAllTranslationsFiles() {
 if [ $# -eq 1 ]; then
     echo "----------------- $(date)"
     echo "Correct all translations ids in $1"
-    correctTranslationsIds $1
+    correctAllTranslationsFiles $1
     echo "----------------- $(date)"
 fi
